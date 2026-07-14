683bbeff6f7fc21f6f1d3bee2d415d72657bbc0e
Komal04-pt <komal104btcse23@igdtuw.ac.in>
2026-07-14 18:14:22 +0530

    fix: remove broken facebook image from gallery

--- server-with-client/client/src/pages/Gallaery/GalleryData.js
+++ server-with-client/client/src/pages/Gallaery/GalleryData.js
@@ -1,10 +1,9 @@
 export const photos = [
   {
-    src: " https://image.made-in-china.com/202f0j00rhBoQJvRYEcH/Furniture-Factory-Custom-Wooden-Small-Arch-Style-L-Shape-3-Person-Clinic-Hospital-Reception-Salon-Modern-Reception-Desks.jpg",
+    src: "https://image.made-in-china.com/202f0j00rhBoQJvRYEcH/Furniture-Factory-Custom-Wooden-Small-Arch-Style-L-Shape-3-Person-Clinic-Hospital-Reception-Salon-Modern-Reception-Desks.jpg",
     width: 800,
     height: 600,
   },
- 
   {
     src: "https://5.imimg.com/data5/SELLER/Default/2024/3/401417614/YI/WO/KC/5104913/hospital-interior-designers.jpg",
     width: 800,
@@ -27,11 +26,6 @@ export const photos = [
   },
   {
     src: "https://suryahospitals.com/hospital/20231107072125-Twins%20Sharing%20Room.jpg",
-    width: 800,
-    height: 600,
-  },
-  {
-    src: "https://scontent.fdel1-7.fna.fbcdn.net/v/t39.30808-6/481084487_1072822471528576_8068540105764407941_n.jpg?stp=cp6_dst-jpg_tt6&cstp=mx1600x1200&ctp=s1600x1200&_nc_cat=109&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=127cfc&_nc_ohc=P3qZSLCLyScQ7kNvwGhp6ix&_nc_oc=AdrIrld180xf6DoSzIQlSDc1cuOMvlZ2o6p-3OvUO8Jmv6GvylK5CC_BZqLfAed65IY&_nc_zt=23&_nc_ht=scontent.fdel1-7.fna&_nc_gid=cAart_ToLnooPvUdTbd7Zg&_nc_ss=7b289&oh=00_AQCCV_8INYs_enrin9N7c_1Q8SEwbzdN_iizz9LUr3KErQ&oe=6A4D231D",
     width: 800,
     height: 600,
   },
