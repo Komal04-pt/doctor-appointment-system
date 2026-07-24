d82a0ccd3b061aab9b6a6ece30c70558ff8cd450
Komal04-pt <komal104btcse23@igdtuw.ac.in>
2026-07-24 18:24:35 +0530

    fix api endpoint path

--- admin-panel/src/redux/actions/authActions.js
+++ admin-panel/src/redux/actions/authActions.js
@@ -1,12 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import API from "../../Api/API";
 
-//LOGIN
 export const login = createAsyncThunk(
   "auth/login",
   async ({ email, password }, thunkApi) => {
     try {
-      const res = await API.post("/user/login", { email, password });
+      const res = await API.post("/api/v1/user/login", { email, password });
       localStorage.setItem("appData", JSON.stringify(res?.data));
       return res?.data;
     } catch (error) {
@@ -17,14 +16,12 @@ export const login = createAsyncThunk(
   }
 );
 
-//get user data
 export const getUserData = createAsyncThunk("auth/getUserData", () => {
   const localData = localStorage.getItem("appData");
   const appData = JSON.parse(localData);
   return appData?.user;
 });
 
-//get token
 export const loadToken = createAsyncThunk("auth/loadToken", () => {
   const localData = localStorage.getItem("appData");
   const appData = JSON.parse(localData);
